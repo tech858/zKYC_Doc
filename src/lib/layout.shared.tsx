4bef0109c270b414f427e4fc61b83793244a9203
Mohamed Zaid <165343185+naoufome@users.noreply.github.com>
2025-12-25 16:48:24 +0100

    update

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -3,7 +3,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'zKYC',
+      title: <span className="font-bold">zKYC</span>,
     },
     links: [],
   };
